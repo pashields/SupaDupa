0e1c715cd6a4154cab930341493cfd339f5ae344
Pat Shields <yeoldefortran@gmail.com>
2013-01-11 17:13:33 -0500

    Add some basic tests on main class.

---
+++ supadupa/test/test_SupaDupa.py
@@ -0,0 +1,69 @@
+import SupaDupa
+from SupaDupa import SupaDupaConf
+import unittest
+
+class SupaDupaConfTest(unittest.TestCase):
+    def setUp(self):
+        self.conf = SupaDupaConf('objc', {'foo': 'bar'}, {'dicty': 'NSDictionary'}, 
+                                 False, None)
+
+    def testKeyMapping(self):
+        self.assertEquals(self.conf.mapKey('bar'), 'bar')
+        self.assertEquals(self.conf.mapKey('foo'), 'bar')
+
+    def testClassOverride(self):
+        self.assertEquals(self.conf.classOverrideForProp('NSDictionary'),
+                          None)
+        self.assertEquals(self.conf.classOverrideForProp('dicty'),
+                          'NSDictionary')
+
+class SupaDupaTest(unittest.TestCase):
+    def setUp(self):
+        self.conf = SupaDupaConf('objc', {'foo': 'bar', 'zip': 'ping'}, 
+                                 {'dicty': 'NSDictionary', 'ping': 'OH NOES'}, 
+                                 False, None)
+
+    def testAddClassFromJsonBasic(self):
+        basicJson = {'property': u'stringValue'}
+        out = SupaDupa.addClassFromJson(basicJson, {}, 'className', self.conf)
+        self.assertEquals(len(out), 1)
+        classDescr = out['className']
+        self.assertEquals(len(classDescr), 1)
+        self.assertEquals(classDescr['property'], 'NSString')
+
+    def testAddClassWithKeyMap(self):
+        json = {u'foo': 1}
+        out = SupaDupa.addClassFromJson(json, {}, u'x', self.conf)
+        self.assertEquals(out[u'x']['bar'], 'NSNumber')
+    
+    def testAddClassWithFixedPropClass(self):
+        json = {u'dicty': 1}
+        out = SupaDupa.addClassFromJson(json, {}, u'x', self.conf)
+        self.assertEquals(out[u'x']['dicty'], 'NSDictionary')
+        json = {u'dicty': {u'foo', 'bar'}}
+        out = SupaDupa.addClassFromJson(json, {}, u'x', self.conf)
+        self.assertEquals(len(out), 1)
+        self.assertEquals(out[u'x']['dicty'], 'NSDictionary')
+        
+    def testMixPropClassAndKeyMap(self):
+        json = {u'zip': 1}
+        out = SupaDupa.addClassFromJson(json, {}, u'x', self.conf)
+        self.assertEquals(out[u'x']['ping'], 'OH NOES')
+
+    def testNestedClass(self):
+        json = {u'inner': {u'stringy': u'xxx'}}
+        out = SupaDupa.addClassFromJson(json, {}, u'x', self.conf)
+        self.assertEquals(len(out), 2)
+        self.assertEquals(out[u'x']['inner'], 'inner')
+        self.assertEquals(out[u'inner']['stringy'], 'NSString')
+
+    def testConvertName(self):
+        self.assertEquals(SupaDupa.convertNameFromArray(u'Plans'), u'Plan')
+        self.assertEquals(SupaDupa.convertNameFromArray(u'Plan'), u'Plan')
+
+    def testArray(self):
+        json = {u'inners': [{u'stringy': u'xxx'}]}
+        out = SupaDupa.addClassFromJson(json, {}, u'x', self.conf)
+        self.assertEquals(len(out), 2)
+        self.assertEquals(out[u'x'][u'inners'], 'NSArray')
+        self.assertEquals(out[u'inner'][u'stringy'], 'NSString')
